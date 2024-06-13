3514938afd5e1136f8424a0a31e19496e0c29126
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-13 22:50:56 +0100

    ops: dry run log aggregation on pipeops platform

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,5 +2,7 @@ import { NextResponse } from "next/server";
 
 /* healthcheck endpoint */
 export async function GET(request: Request) {
+    const origin = request.headers.get("x-forwarded-for");
+    console.log("healthcheck request, at %s from %s", new Date(), origin);
     return NextResponse.json({ message: "okay" });
 }
