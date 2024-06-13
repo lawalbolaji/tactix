01ef979c79c074113a12ac3f3a43a9ead07029ee
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-13 23:32:54 +0100

    Add typewriter effect to welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <div className="text-7xl font-bold">Welcome to Tactix ⚙️</div>
+            <div className="text-7xl font-bold line-1 anim-typewriter">Welcome to Tactix</div>
         </main>
     );
 }
