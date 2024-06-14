caa3a71742c1b8bb69682518b890a6a4b17f67c4
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-14 07:46:11 +0100

    cleanup typewriter effect in text
    - also add go to dashboard button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <div className="text-7xl font-bold line-1 anim-typewriter">Welcome to Tactix</div>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center p-4 lg:p-24">
+            <div className="text:3xl lg:text-7xl font-bold line-1 anim-typewriter">Welcome to Tactix</div>
+            <div className="lg:mt-8 mt-4 launch transition ease-in-out hover:-translate-y-1 hover:scale-110">
+                <Link
+                    href={"/dashboard"}
+                    className="flex items-center h-full w-full justify-center font-bold mt-4 p-2 lg:p-4 launch border border-blue-200 rounded-lg hover:bg-blue-200"
+                >
+                    Go 🚀
+                </Link>
+            </div>
         </main>
     );
 }
