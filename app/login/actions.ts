5bf71b70e674eecab9dae20c3ec5dd25e20e28d1
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-15 09:57:19 +0100

    plug personal data leak

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -15,10 +15,7 @@ export async function login(formData: FormData) {
         password: formData.get("password") as string,
     };
 
-    console.log(data);
-
     const { error } = await supabase.auth.signInWithPassword(data);
-
     if (error) {
         console.log(error);
 
