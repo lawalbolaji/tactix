bf363c6041c6b4919274a905986e7619e6b1f677
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-18 06:21:58 +0100

    add background image to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen w-full flex-col items-center justify-center p-4 lg:p-24">
+        <main className="flex min-h-screen w-full flex-col items-center justify-center p-4 lg:p-24 lg:bg-[url('/tactix.png')] lg:bg-cover lg:bg-no-repeat lg:bg-center bg-transparent">
             <div className="text:3xl lg:text-7xl font-bold line-1 anim-typewriter">Welcome to Tactix</div>
             <div className="lg:mt-8 mt-4 launch transition ease-in-out hover:-translate-y-1 hover:scale-110">
                 <Link
