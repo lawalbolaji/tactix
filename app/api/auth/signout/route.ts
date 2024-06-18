ac82020f2221c9a350e70e70d7b765c49aaeca31
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-18 06:22:10 +0100

    create signout route

---
+++ app/api/auth/signout/route.ts
@@ -0,0 +1,13 @@
+import { revalidatePath } from "next/cache";
+import { createClient } from "../../../../lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut();
+
+    if (error) console.log(error);
+
+    revalidatePath("/v2/dashboard", "layout");
+    redirect("/");
+}
