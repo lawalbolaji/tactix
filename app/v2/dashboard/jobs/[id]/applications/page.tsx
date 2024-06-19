9b4585c92fa4025688e75b73f3c48d7b2176aeb2
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-19 07:38:05 +0100

    theme: remove dark mode selectors tailwind

--- app/v2/dashboard/jobs/[id]/applications/page.tsx
+++ app/v2/dashboard/jobs/[id]/applications/page.tsx
@@ -109,7 +109,7 @@ export default async function ApplicationsPage({ params }: { params: { id: strin
                         <div className="flex justify-center">
                             <Link
                                 href={`./applications/all`}
-                                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
                                 prefetch={false}
                             >
                                 View All Applicants
--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -61,10 +61,10 @@ export function SignIn() {
 
                 <div className="relative">
                     <div className="absolute inset-0 flex items-center">
-                        <div className="w-full border-t border-gray-300 dark:border-gray-700" />
+                        <div className="w-full border-t border-gray-300" />
                     </div>
                     <div className="relative flex justify-center text-sm">
-                        <span className="bg-white px-2 text-gray-500 dark:bg-gray-950 dark:text-gray-400">
+                        <span className="bg-white px-2 text-gray-500">
                             or continue with
                         </span>
                     </div>
