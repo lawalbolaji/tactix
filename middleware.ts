a3235478d58ecc8ad9dc5781d5be4a198268d8c9
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-20 13:56:49 +0100

    - fix issue with supabase auth causing refresh token reuse error;
    I believe the error was happening because the middleware was updating session even on the login route

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,17 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === "/" || request.nextUrl.pathname === "/v2/register") return NextResponse.next();
+    console.log(
+        "auth session refreshed from %s",
+        request.headers.get("x-forwarded-for") || request.headers.get("x-real-ip")
+    );
+
     return await updateSession(request);
 }
 
 export const config = {
+    // matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
     matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
 };
