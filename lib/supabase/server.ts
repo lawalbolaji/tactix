9b079265b4273db6cc2528ae5b0d634437c8e4ec
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-07-03 10:57:00 +0100

    fix: dynamic_server_usage vercel

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,14 +1,8 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { cookies } from "next/headers";
 
 export function createClient() {
-    let cookieStore: ReadonlyRequestCookies;
-    try {
-        cookieStore = cookies();
-    } catch (error) {
-        console.log(error);
-    }
+    const cookieStore = cookies();
 
     return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
         cookies: {
