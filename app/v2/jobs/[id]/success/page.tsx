ffd8a45f94f6c4fc0012aa3187fad09a95777c66
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-07-04 02:10:02 +0100

    center success message vertically

--- app/v2/jobs/[id]/success/page.tsx
+++ app/v2/jobs/[id]/success/page.tsx
@@ -1,5 +1,5 @@
 export default function SuccessPage() {
     return (
-        <div className="flex items-center justify-center h-screen">Your application has been sent successfully! ✅</div>
+        <div className="flex items-center justify-center h-[60vh]">Your application has been sent successfully! ✅</div>
     );
 }
