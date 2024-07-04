f255a662278dd2c30598a39665985876028e8d44
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-07-04 16:49:37 +0100

    feat: add tooltips to copy, delete and publish icons on all jobs page

--- components/jobs/delete.tsx
+++ components/jobs/delete.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { TrashIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 type remoteRequestStatus = "idle" | "loading" | "success" | "error";
 
@@ -11,51 +12,60 @@ export function DeleteJobButton(props: { jobId: number }) {
     const [remoteRequestStatus, setRemoteRequestStatus] = useState<remoteRequestStatus>("idle");
 
     return (
-        <Button
-            variant="ghost"
-            size="icon"
-            className="text-gray-500 hover:text-red-400"
-            aria-disabled={remoteRequestStatus === "loading"}
-            disabled={remoteRequestStatus === "loading"}
-            onClick={async () => {
-                setRemoteRequestStatus("loading");
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="text-gray-500 hover:text-red-400"
+                        aria-disabled={remoteRequestStatus === "loading"}
+                        disabled={remoteRequestStatus === "loading"}
+                        onClick={async () => {
+                            setRemoteRequestStatus("loading");
 
-                try {
-                    const response = await fetch(`/api/jobs/${props.jobId}`, {
-                        method: "POST",
-                        cache: "no-cache",
-                        body: JSON.stringify({ op: "SOFT_DELETE" }),
-                        next: { revalidate: 0 },
-                    });
+                            try {
+                                const response = await fetch(`/api/jobs/${props.jobId}`, {
+                                    method: "POST",
+                                    cache: "no-cache",
+                                    body: JSON.stringify({ op: "SOFT_DELETE" }),
+                                    next: { revalidate: 0 },
+                                });
 
-                    if (response.ok) {
-                        setRemoteRequestStatus("success");
+                                if (response.ok) {
+                                    setRemoteRequestStatus("success");
 
-                        /* refresh page - https://stackoverflow.com/questions/3715047/how-to-reload-a-page-using-javascript#:~:text=page%20for%20more-,information,-.*/
-                        window.location.reload();
-                        return false;
-                    }
+                                    /* refresh page - https://stackoverflow.com/questions/3715047/how-to-reload-a-page-using-javascript#:~:text=page%20for%20more-,information,-.*/
+                                    window.location.reload();
+                                    return false;
+                                }
 
-                    const responseAsJson = await response.json();
-                    setRemoteRequestStatus("error");
+                                const responseAsJson = await response.json();
+                                setRemoteRequestStatus("error");
 
-                    /* DEBUGGING */
-                    console.log(responseAsJson);
-                } catch (error) {
-                    // fetch request is basically not working - network issues?
-                    console.error(error);
-                    setRemoteRequestStatus("error");
-                }
-            }}
-        >
-            {remoteRequestStatus === "loading" ? (
-                <div className="h-5 w-5 animate-spin rounded-full border-4 border-gray-300 border-t-[#d099d750]" />
-            ) : (
-                <>
-                    <TrashIcon className="w-5 h-5" />
-                    <span className="sr-only">Publish</span>
-                </>
-            )}
-        </Button>
+                                /* DEBUGGING */
+                                console.log(responseAsJson);
+                            } catch (error) {
+                                // fetch request is basically not working - network issues?
+                                console.error(error);
+                                setRemoteRequestStatus("error");
+                            }
+                        }}
+                    >
+                        {remoteRequestStatus === "loading" ? (
+                            <div className="h-5 w-5 animate-spin rounded-full border-4 border-gray-300 border-t-[#d099d750]" />
+                        ) : (
+                            <>
+                                <TrashIcon className="w-5 h-5" />
+                                <span className="sr-only">Publish</span>
+                            </>
+                        )}
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Delete job</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
     );
 }
--- components/jobs/publish.tsx
+++ components/jobs/publish.tsx
@@ -3,6 +3,7 @@
 import { Rss } from "lucide-react";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 type remoteRequestStatus = "idle" | "loading" | "success" | "error";
 
@@ -11,50 +12,59 @@ export function PublishJobButton(props: { jobId: number; is_published: boolean }
     const [remoteRequestStatus, setRemoteRequestStatus] = useState<remoteRequestStatus>("idle");
 
     return (
-        <Button
-            variant="ghost"
-            size="icon"
-            className="text-gray-500 hover:text-gray-900"
-            aria-disabled={remoteRequestStatus === "loading" || props.is_published}
-            disabled={remoteRequestStatus === "loading" || props.is_published}
-            onClick={async () => {
-                setRemoteRequestStatus("loading");
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="text-gray-500 hover:text-gray-900"
+                        aria-disabled={remoteRequestStatus === "loading" || props.is_published}
+                        disabled={remoteRequestStatus === "loading" || props.is_published}
+                        onClick={async () => {
+                            setRemoteRequestStatus("loading");
 
-                try {
-                    const response = await fetch(`/api/jobs/${props.jobId}`, {
-                        method: "POST",
-                        cache: "no-cache",
-                        body: JSON.stringify({ op: "PUBLISH" }),
-                    });
+                            try {
+                                const response = await fetch(`/api/jobs/${props.jobId}`, {
+                                    method: "POST",
+                                    cache: "no-cache",
+                                    body: JSON.stringify({ op: "PUBLISH" }),
+                                });
 
-                    if (response.ok) {
-                        setRemoteRequestStatus("success");
+                                if (response.ok) {
+                                    setRemoteRequestStatus("success");
 
-                        /* refresh page */
-                        window.location.reload();
-                        return false;
-                    }
+                                    /* refresh page */
+                                    window.location.reload();
+                                    return false;
+                                }
 
-                    const responseAsJson = await response.json();
-                    setRemoteRequestStatus("error");
+                                const responseAsJson = await response.json();
+                                setRemoteRequestStatus("error");
 
-                    /* DEBUGGING */
-                    console.log(responseAsJson);
-                } catch (error) {
-                    // fetch request is basically not working - network issues?
-                    console.error(error);
-                    setRemoteRequestStatus("error");
-                }
-            }}
-        >
-            {remoteRequestStatus === "loading" ? (
-                <div className="h-5 w-5 animate-spin rounded-full border-4 border-gray-300 border-t-[#d099d750]" />
-            ) : (
-                <>
-                    <Rss className="w-5 h-5" />
-                    <span className="sr-only">Publish</span>
-                </>
-            )}
-        </Button>
+                                /* DEBUGGING */
+                                console.log(responseAsJson);
+                            } catch (error) {
+                                // fetch request is basically not working - network issues?
+                                console.error(error);
+                                setRemoteRequestStatus("error");
+                            }
+                        }}
+                    >
+                        {remoteRequestStatus === "loading" ? (
+                            <div className="h-5 w-5 animate-spin rounded-full border-4 border-gray-300 border-t-[#d099d750]" />
+                        ) : (
+                            <>
+                                <Rss className="w-5 h-5" />
+                                <span className="sr-only">Publish</span>
+                            </>
+                        )}
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Publish job</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
     );
 }
